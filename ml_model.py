7ecabb8d2a642e9675d031bca03093a81b0e4cd7
SreevalliK <ksreevalli55@gmail.com>
2021-11-20 20:48:57 -0500

    Chnaged for conflicts

--- ml_model.py
+++ ml_model.py
@@ -6,7 +6,7 @@ def model1():
     print("Adding code for Model1")
 
 def model2():
-    print("Adding code for Model2222")
+    print("Changed code for conflicts")
 
 def model3():
     print("Adding code for model33333")
