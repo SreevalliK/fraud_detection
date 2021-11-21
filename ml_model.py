271c01bde18c8eeaad36c2813423f2835c726159
SreevalliK <ksreevalli55@gmail.com>
2021-11-20 20:27:32 -0500

    Added model functions

--- ml_model.py
+++ ml_model.py
@@ -1,3 +1,12 @@
 
 
 print("Adding all ML related code")
+
+def model1():
+    print("Adding code for Model1")
+
+def model2():
+    print("Adding code for Model2")
+
+def model3():
+    print("Adding code for model3")
