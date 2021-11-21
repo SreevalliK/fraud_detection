491183c31b9a1d41d115f8ad2774dda3cf4b0634
SreevalliK <ksreevalli55@gmail.com>
2021-11-20 20:39:00 -0500

    Make changes to model2 function

--- ml_model.py
+++ ml_model.py
@@ -6,7 +6,7 @@ def model1():
     print("Adding code for Model1")
 
 def model2():
-    print("Adding code for Model2")
+    print("Adding code for Model2222")
 
 def model3():
     print("Adding code for model3")
